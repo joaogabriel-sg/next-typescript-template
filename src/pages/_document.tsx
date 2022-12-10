5850068054bfb10d49255a535f7fa4a8e9c85d1a
João Gabriel <main.joaogabrielsg@gmail.com>
2022-12-09 21:57:48 -0300

    feat: create _document file

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function MyDocument() {
+  return (
+    <Html lang="pt-BR">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
