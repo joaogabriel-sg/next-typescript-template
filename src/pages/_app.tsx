32f7d28f532496b2b15a0eae151b82548341adab
João Gabriel <main.joaogabrielsg@gmail.com>
2022-12-09 21:57:10 -0300

    feat: add meta tags to pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Next TypeScript Template</title>
+        <link rel="shortcut icon" href="/images/icon-512.png" />
+        <link rel="apple-touch-icon" href="/images/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="description"
+          content="A simple Create Next App template to start your projects with Next.js, TypeScript, Tailwind CSS, ESLint, Prettier and other tools."
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
