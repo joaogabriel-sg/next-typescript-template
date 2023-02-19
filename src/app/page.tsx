9dd05a41abc46d6c09ef74cbc45eeb7c277dfb76
João Gabriel <main.joaogabrielsg@gmail.com>
2023-02-19 17:29:33 -0300

    feat: add app dir

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="text-4xl font-black text-white">Hello World!</h1>
+    </div>
+  );
+}
