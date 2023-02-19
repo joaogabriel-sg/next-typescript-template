a6b36c770fee02dfddd00cc1c08b61dcb4b3576a
João Gabriel <main.joaogabrielsg@gmail.com>
2023-02-19 17:30:20 -0300

    chore: add app dir configs

--- next.config.js
+++ next.config.js
@@ -10,6 +10,9 @@ const withPWA = require('next-pwa')({
 const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
 });
 
 module.exports = nextConfig;
