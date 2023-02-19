7792b9ba9b8c373948cf3ca7b833328aee1f15be
João Gabriel <main.joaogabrielsg@gmail.com>
2023-02-19 17:34:44 -0300

    chore: remove next-pwa configs

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
-
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: !isProd,
-});
-
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     appDir: true,
   },
-});
+};
 
 module.exports = nextConfig;
